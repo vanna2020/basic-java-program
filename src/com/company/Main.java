1bf130bb535678b6b0d6ef8abbc3c1f3f3a5ebd3
vanna2020 <vannasingh1999@gmail.com>
2021-09-09 03:33:20 -0700

    swaping two number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 
              public class Main {
 
-                             public static void main(String[] args) {
+                                     public static void main(String[] args) {
+                                         int a, b;
+                                         Scanner sc = new Scanner(System.in);
+                                         System.out.print("Enter the value of a : ");
+                                         a = sc.nextInt();
+                                         System.out.print("Enter the value of b : ");
+                                         b = sc.nextInt();
+                                         int t = 0;
+                                         // Taking 1 additional variable t to swap
+                                         t = a;
+                                         a = b;
+                                         b = t;
 
-                                 int quotient, remainder, dividend, divisor;
-                                 Scanner scanner = new Scanner(System.in);
-                                 // value which has to divide
-                                 System.out.print("Enter Dividend value : ");
-                                 dividend = scanner.nextInt();
-                                 // the value to  divide by
-                                 System.out.print("Enter Divisor value : ");
-                                 divisor = scanner.nextInt();
-                                 //arithmetic operation
-                                 quotient = dividend / divisor;
-                                 remainder = dividend % divisor;
-                                 System.out.println("Quotient = " + quotient);
-                                 System.out.println("Remainder= " + remainder);
-                             }
-                         }
+                                         System.out.println("The swap value of a is : " + a);
+                                         System.out.println("The swap value of b is : " + b);
+                                     }
+                                 }
