6542aaafcaad19fcd73de8e5a321566a8e02f3d6
vanna2020 <vannasingh1999@gmail.com>
2021-09-09 03:45:33 -0700

    check largest no

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,44 +3,24 @@ import java.util.Scanner;
 
              public class Main {
           public static void main(String[] args) {
-                                 char character;
-                                 Scanner scanner = new Scanner(System.in);
-                                 System.out.print("Enter a Character : ");
-                                 character = scanner.next().charAt(0);
-                                 switch (character) {
-                                     case 'a':
-                                         System.out.println(character + " : is an Vowel");
-                                         break;
-                                     case 'e':
-                                         System.out.println(character + " : is an Vowel");
-                                         break;
-                                     case 'i':
-                                         System.out.println(character + " : is an Vowel");
-                                         break;
-                                     case 'o':
-                                         System.out.println(character + " : is an Vowel");
-                                         break;
-                                     case 'u':
-                                         System.out.println(character + " : is an Vowel");
-                                         break;
-                                     case 'A':
-                                         System.out.println(character + " : is an Vowel");
-                                         break;
-                                     case 'E':
-                                         System.out.println(character + " : is an Vowel");
-                                         break;
-                                     case 'I':
-                                         System.out.println(character + " : is an Vowel");
-                                         break;
-                                     case 'O':
-                                         System.out.println(character + " : is an Vowel");
-                                         break;
-                                     case 'U':
-                                         System.out.println(character + " + : is an Vowel");
-                                         break;
-                                     default:
-                                         System.out.println(character + " : is an Consonent");
-
-                                 }
-                     }
-             }
+                      int number1, number2, number3;
+                      System.out.println("We have to check largest among these three numbers below ");
+                      Scanner sc = new Scanner(System.in);
+                      System.out.print("Enter First number : ");
+                      number1 = sc.nextInt();
+                      System.out.print("Enter Second number : ");
+                      number2 = sc.nextInt();
+                      System.out.print("Enter Third number : ");
+                      number3 = sc.nextInt();
+                      //
+                      if (number1>number2 && number1>number3){
+                          System.out.println(number1 + "  is largest in " + number1 + "," + number2 + "," + number3);
+                      }
+                      else if (number2>number1 && number2>number3){
+                          System.out.println(number2 + "  is largest in " + number1 + "," + number2 + "," + number3);
+                      }
+                      else {
+                          System.out.println(number3 + "  is largest in " + number1 + "," + number2 + "," + number3);
+                      }
+                  }
+              }
