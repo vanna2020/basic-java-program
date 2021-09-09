67b867d47f8984ec09e785b20336a68e5a675fb7
vanna2020 <vannasingh1999@gmail.com>
2021-09-09 03:36:57 -0700

    check even or odd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,16 @@ import java.util.Scanner;
 
              public class Main {
 
-                                     public static void main(String[] args) {
-                                         int a, b;
-                                         Scanner sc = new Scanner(System.in);
-                                         System.out.print("Enter the value of a : ");
-                                         a = sc.nextInt();
-                                         System.out.print("Enter the value of b : ");
-                                         b = sc.nextInt();
-                                         int t = 0;
-                                         // Taking 1 additional variable t to swap
-                                         t = a;
-                                         a = b;
-                                         b = t;
 
-                                         System.out.println("The swap value of a is : " + a);
-                                         System.out.println("The swap value of b is : " + b);
-                                     }
-                                 }
+                     public static void main(String[] args) {
+                         Scanner sc = new Scanner(System.in);
+                         System.out.print("Enter a number to check even or odd : ");
+                         int num = sc.nextInt();
+                         if (num % 2 == 0){
+                             System.out.println(num + " : is an even number");
+                         }
+                         else {
+                             System.out.println(num + " : is an odd number");
+                         }
+                     }
+                 }
