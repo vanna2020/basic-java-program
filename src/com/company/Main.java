488ff700ee0ac9697007eacaf8dbc8b895e97a7f
vanna2020 <vannasingh1999@gmail.com>
2021-09-09 03:19:18 -0700

    harmonic no

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,21 @@ import java.util.Scanner;
 
              public class Main {
 
-                 static final int MAX_VALUE = 32;
-                 static final int MIN_VALUE = 0;
-
-                 public static void main(String[] args) {
-                     //Variables
-                     int number;
-                     int power;
-                     System.out.print("Enter a number to Print 2 power Table : ");
-                     Scanner scanner = new Scanner(System.in);
-                     number = scanner.nextInt();
-                     // checking the number in the 0 to 32 range , to not encounter overflow
-                     if (number >= MIN_VALUE && number < MAX_VALUE) {
-                         for (int i = 0; i < number; i++) {
-                             power = (int) Math.pow(2, i);
-                             System.out.println("2" + "^" + i + "=" + power);
+                     public static void main(String[] args) {
+                         //variables  harmonic expression starts with 1 always
+                         Float harmonicNumber = 1f;
+                         int number;
+                         System.out.print("Enter the number to Check Harmonic expression (1+ 1/2 + 1/3 + ....+ 1/N) : ");
+                         Scanner scanner = new Scanner(System.in);
+                         number = scanner.nextInt();
+                         //execute only if number >0
+                         if (number != 0) {
+                             for (int i = 2; i <= number; i++) {
+                                 System.out.println("i value:" + i);
+                                 harmonicNumber += (float) 1 / i;
+                                 System.out.println(harmonicNumber);
+                             }
+                             System.out.print("Harmonic Value of Number " + number + " is :  " + harmonicNumber);
                          }
                      }
                  }
-             }
-
-
-
