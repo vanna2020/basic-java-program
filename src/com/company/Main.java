fb79e791c281eeb04b288436449ec8ca421108b4
vanna2020 <vannasingh1999@gmail.com>
2021-09-09 03:28:30 -0700

    quotient and reminder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,20 @@ import java.util.Scanner;
 
              public class Main {
 
-                     public static void main(String[] args) {
+                             public static void main(String[] args) {
 
-                                 Scanner scan = new Scanner(System.in);
-                                 System.out.print("Input a number: ");
-                                 int number = scan.nextInt();
-                                 if (number > 0) {
-                                     while (number % 2 == 0) {
-                                         System.out.print(2 + " ");
-                                         number /= 2;
-                                     }
-
-                                     for (int i = 3; i <= Math.sqrt(number); i += 2) {
-                                         while (number % i == 0) {
-                                             System.out.print(i + " ");
-                                             number /= i;
-                                         }
-                                     }
-                                     if (number > 2)
-                                         System.out.print(number);
-                                 }
+                                 int quotient, remainder, dividend, divisor;
+                                 Scanner scanner = new Scanner(System.in);
+                                 // value which has to divide
+                                 System.out.print("Enter Dividend value : ");
+                                 dividend = scanner.nextInt();
+                                 // the value to  divide by
+                                 System.out.print("Enter Divisor value : ");
+                                 divisor = scanner.nextInt();
+                                 //arithmetic operation
+                                 quotient = dividend / divisor;
+                                 remainder = dividend % divisor;
+                                 System.out.println("Quotient = " + quotient);
+                                 System.out.println("Remainder= " + remainder);
                              }
                          }
