1542240fe66ca21f8f8d2d229030e6bd17c62fbe
vanna2020 <vannasingh1999@gmail.com>
2021-09-09 03:41:20 -0700

    check vowel or consonant

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,45 @@ package com.company;
 import java.util.Scanner;
 
              public class Main {
+          public static void main(String[] args) {
+                                 char character;
+                                 Scanner scanner = new Scanner(System.in);
+                                 System.out.print("Enter a Character : ");
+                                 character = scanner.next().charAt(0);
+                                 switch (character) {
+                                     case 'a':
+                                         System.out.println(character + " : is an Vowel");
+                                         break;
+                                     case 'e':
+                                         System.out.println(character + " : is an Vowel");
+                                         break;
+                                     case 'i':
+                                         System.out.println(character + " : is an Vowel");
+                                         break;
+                                     case 'o':
+                                         System.out.println(character + " : is an Vowel");
+                                         break;
+                                     case 'u':
+                                         System.out.println(character + " : is an Vowel");
+                                         break;
+                                     case 'A':
+                                         System.out.println(character + " : is an Vowel");
+                                         break;
+                                     case 'E':
+                                         System.out.println(character + " : is an Vowel");
+                                         break;
+                                     case 'I':
+                                         System.out.println(character + " : is an Vowel");
+                                         break;
+                                     case 'O':
+                                         System.out.println(character + " : is an Vowel");
+                                         break;
+                                     case 'U':
+                                         System.out.println(character + " + : is an Vowel");
+                                         break;
+                                     default:
+                                         System.out.println(character + " : is an Consonent");
 
-
-                     public static void main(String[] args) {
-                         Scanner sc = new Scanner(System.in);
-                         System.out.print("Enter a number to check even or odd : ");
-                         int num = sc.nextInt();
-                         if (num % 2 == 0){
-                             System.out.println(num + " : is an even number");
-                         }
-                         else {
-                             System.out.println(num + " : is an odd number");
-                         }
+                                 }
                      }
-                 }
+             }
